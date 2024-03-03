04197ef13620e6d484ca33cf0fdf582623396e31
deranderemark <mark.steffes04@gmail.com>
2024-03-03 17:00:52 +0100

    changed domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import preact from "@astrojs/preact";
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact()],
-  site: 'https://astro-tutorial.marksteffes.de',
+  site: 'https://marksteffes.de',
 });
